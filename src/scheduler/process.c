e7773fe68a64f0b289b9f3b87a7b6b8c75b51d17
mpsubiabre <you@example.com>
2021-09-16 16:22:52 -0300

    se termino la funcion de prioridades

--- src/scheduler/process.c
+++ src/scheduler/process.c
@@ -9,8 +9,26 @@ Process* process_init(int pid, char* name, int id_factory, int time_start){
     process->state = READY;
     process->next = NULL;
     process->time_start = time_start;
-    process->quantum = 0;
+    process->quantum = -1;
     process->t_entrada_cpu = 0;
     process->puntero_rafaga = 0;
     return process;
+}
+
+void print_proces(Process* process, int timer){
+    switch (process->state)
+    {
+    case RUNNING:
+        printf("[t = %i] El proceso %s ha pasado a RUNNING",timer, process->name);
+        break;
+    case READY:
+        printf("[t = %i] El proceso %s ha pasado a READY",timer, process->name);
+        break;
+    case WAITING:
+        printf("[t = %i] El proceso %s ha pasado a WAITING",timer, process->name);
+        break;
+    case FINISHED:
+        printf("[t = %i] El proceso %s ha pasado a FINISHED",timer, process->name);
+        break;
+    }
 }
--- src/scheduler/scheduler.c
+++ src/scheduler/scheduler.c
@@ -47,84 +47,42 @@ void destroy_linkedlist(LinkedList* linkedlist)
     }
     free(linkedlist);
 }
-/*Rec0gido de geeksforgeeks para eliminar un nodo de la lista
-https://www.geeksforgeeks.org/delete-a-given-node-in-linked-list-under-given-constraints/
-*/
-void delete_process(LinkedList* linkedlist ,Process* current){
-    Process* head = linkedlist->head;
-    // es el primer nodo de la lista
-    if(head == current)
-    {
-        if(head->next == NULL)
-        {
-           //SOLO HAY UN NODO
-           //Falta borrarlo
-           return; 
-        }
-        /*Se copia la info del nodo siguiente al head*/
-        head->id_factory=head->next->id_factory;
-        head->name =head->next->name;
-        head->pid=head->next->pid;
-        head->quantum=head->next->quantum;
-        head->state=head->next->state;
-        head->time_start=head->next->time_start;
-        //agregar atributos
-        /*Se guarda la direccion*/
-        current = head->next;
-        //Se remueve el link del siguiente nodo
-        head->next = head->next->next;
-        
-        free(current);
-        return;
-    }
-    // No es el primer nodo
-    Process* prev = head;
-    while (prev->next != NULL && prev->next!=current)
-    {
-        prev = prev->next;
-    }
-    if(prev->next == NULL){
-        //NO esta presente en la lista
-        return;
-    }
-    prev->next = prev->next->next;
-    free(current);
-    return;
-}
-
-
 
 Process* delete_process_pachi(LinkedList* linkedlist ,Process* current){
-    printf("entre a la funcion delete proces pachi con current %s , cabeza %s y cola %s\n", current-> name, linkedlist->head->name, linkedlist->tail->name);
+    printf("entre a la funcion delete process con current %s , cabeza %s y cola %s\n", current-> name, linkedlist->head->name, linkedlist->tail->name);
     // es el primer nodo de la lista
     if(linkedlist->head == current)
     {
-        printf("es el primer nodo de la lista\n");
+        //printf("es el primer nodo de la lista\n");
         if(linkedlist->head->next == NULL)//SOLO HAY UN NODO
         {
-           printf("solo hay un nodo\n");
+           //printf("solo hay un nodo\n");
            linkedlist->head = NULL; //la dejo vacia
+           current->next = NULL;
            return current; 
         }
         //hay mas de un nodo
-        printf("hay mas de un nodo\n");
+        //printf("hay mas de un nodo\n");
         linkedlist->head = linkedlist->head->next;// la cabeza ahora es el hermano de la cabeza original
+        current->next = NULL;
         return current;
     }
     // No es el primer nodo
-    printf("no es el primer nodo\n");
+    //printf("no es el primer nodo\n");
     Process* nodo = linkedlist->head;
     while (nodo->next != NULL && nodo->next!=current)
     {   
-        printf("entre al while\n");
+        //printf("entre al while\n");
         nodo = nodo->next;
     }
-    printf("sali del while\n");
+    //printf("sali del while\n");
     if(nodo->next == NULL){
         //NO esta presente en la lista
+
         return current;
     }
     nodo->next = nodo->next->next;
     //free(current); maybe free nodo
+    current->next = NULL;
     return current;
 }
